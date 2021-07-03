3f1a2b70ed099d04e9fcc35df820215395468eb3
anesbittjr@aol.com <anesbittjr@aol.com>
2021-07-03 14:05:02 -0400

    the code works I can stop crying now

---
+++ js/app.js
@@ -0,0 +1,79 @@
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('adventure')
+
+let state = {}
+
+function startGame() {
+  state = {}
+  showTextNode(1)
+}
+
+function showTextNode(textDex) {
+  const textNode = textNodes.find(textNode => textNode.id === textDex)
+  textElement.innerText = textNode.text
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+  }
+
+  textNode.options.forEach(option => {
+    if (showOption(option)) {
+      const button = document.createElement('button')
+      button.innerText = option.text
+      button.classList.add('action')
+      button.addEventListener('click', () => selectOption(option))
+      optionButtonsElement.appendChild(button)
+    }
+  })
+}
+
+function showOption(option) {
+  return option.requiredState == null || option.requiredState(state)
+}
+
+function selectOption(option) {
+  const nextTextNodeId = option.nextText
+  if (nextTextNodeId <= 0) {
+    return startGame()
+  }
+  state = Object.assign(state, option.setState)
+  showTextNode(nextTextNodeId)
+}
+
+const textNodes = [
+  {
+    id: 1,
+    text: 'You have entered the House of 1000 Mysteries. What will you do?',
+    options: [
+      {
+        text: 'Stay',
+        nextText: 2
+      },
+      {
+        text: 'Leave',
+        nextText: 20
+      }
+    ]
+  },
+  {
+    id: 20,
+    text: 'You win I guess?',
+    options: [
+      {
+        text: 'Restart',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 2,
+    text: 'The code works!',
+    options: [
+      {
+      text: 'Restart for now',
+      nextText: -1
+      }
+    ]
+  },
+]
+
+startGame()
